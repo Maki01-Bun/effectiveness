039b0ccda18b9a90455b37ad27d36842d0cb6662
Maki01-Bun <marcnelsonpadilla7@gmail.com>
2026-07-19 14:39:49 +0800

    updated

--- app.py
+++ app.py
@@ -1,32 +1,41 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI
+from pydantic import BaseModel
 import joblib
-import pandas as pd
 
-app = Flask(__name__)
+app = FastAPI()
 
-model = joblib.load('random_forest_subsidy.pkl')
+model = joblib.load("random_forest_subsidy.pkl")
 
-@app.route('/predict', methods=['POST'])
-def predict():
+class PredictionInput(BaseModel):
+    subsidy_type: int
+    farm_size: float
+    crop_yield_before: float
+    crop_yield_after: float
+    income_before: float
+    income_after: float
+    pest: int
+    calamity: int
 
-    data = request.json
+@app.get("/")
+def home():
+    return {"message": "AgriSubsidy ML API Running"}
+
+@app.post("/predict")
+def predict(data: PredictionInput):
 
     features = [[
-        data['subsidy_type'],
-        data['farm_size'],
-        data['crop_yield_before'],
-        data['crop_yield_after'],
-        data['income_before'],
-        data['income_after'],
-        data['pest'],
-        data['calamity']
+        data.subsidy_type,
+        data.farm_size,
+        data.crop_yield_before,
+        data.crop_yield_after,
+        data.income_before,
+        data.income_after,
+        data.pest,
+        data.calamity
     ]]
 
     prediction = model.predict(features)
 
-    return jsonify({
-        'effectiveness': str(prediction[0])
-    })
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
+    return {
+        "effectiveness": str(prediction[0])
+    }
